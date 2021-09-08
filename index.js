d1054a3402fc71e5203a9c8e4876ce3ca9b3da9e
Tai Pham <megaxayda@gmail.com>
2021-09-08 11:52:57 +0700

    init

---
+++ index.js
@@ -0,0 +1,69 @@
+const puppeteer = require("puppeteer");
+
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox"],
+    userDataDir: "/tmp/myChromeSession",
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.twitch.tv/directory/game/Genshin%20Impact?sort=VIEWER_COUNT_ASC",
+    { waitUntil: "networkidle2" }
+  );
+
+  //   while (true) {}
+
+  //============NAVIGATE==============
+  //   await page.waitForSelector(`div[style="order: 0;"] a`);
+  for (let i = 0; i < 1000; i++) {
+    console.log("step 1");
+    const channel = `div[style="order: ${i};"] a`;
+    await page.waitForSelector(channel);
+    const link = await page.$(channel); // declare object
+    const newPagePromise = new Promise((x) =>
+      browser.once("targetcreated", (target) => x(target.page()))
+    ); // declare promise
+    await link.click({ button: "middle" }); // click middle button, link open in a new tab
+    const page2 = await newPagePromise; // declare new tab, now you can work with it
+    await page2.waitForTimeout(3000);
+    await page2.bringToFront(); // make the tab active
+
+    console.log("step 2");
+
+    await page2.waitForTimeout(6000);
+    await page2.evaluate((_) => {
+      const button = document.querySelector(
+        '[data-test-selector="follow-button"]'
+      );
+      if (button) {
+        console.log("clicked");
+        button.click();
+      }
+    });
+
+    console.log("step 3");
+    await page2.waitForTimeout(3000);
+
+    console.log("step 4");
+    await page2.goto("about:blank");
+    await page2.waitForTimeout(1000);
+    await page2.close();
+
+    console.log("step 5");
+    await page.bringToFront(); // make the tab active
+    await page.waitForTimeout(1000);
+    if (i % 2 === 0) {
+      await page.evaluate((_) => {
+        window.scrollBy(0, i * 100);
+      });
+    }
+    await page.waitForTimeout(i + 100);
+  }
+
+  await browser.close();
+}
+
+main().catch((e) => {
+  console.log(e);
+});
